240b8b833de0bee177ccdd35f3a6dbef11297442
Lager <richard.stenman1993@gmail.com>
2020-02-01 18:25:17 +0100

    Car physics and model, SteamVR plugin

---
+++ not-mario-kart/Assets/Scripts/WheelController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    public WheelCollider Collider;
+    public Transform Model;
+
+    public void Update()
+    {
+        Vector3 position;
+        Quaternion rotation;
+        this.Collider.GetWorldPose(out position, out rotation);
+        //Vector3 rotatedVector = new Vector3(0, 90, 0);
+
+
+        this.Model.rotation = rotation;
+    }
+}
