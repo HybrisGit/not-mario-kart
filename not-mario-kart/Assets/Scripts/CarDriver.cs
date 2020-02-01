ac177c6dec2ee9aa1e1933ae7cdda7d3984e2be5
Lager <richard.stenman1993@gmail.com>
2020-02-01 19:17:01 +0100

    AI, more steamvr

---
+++ not-mario-kart/Assets/Scripts/CarDriver.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CarComponent))]
+public class CarDriver : MonoBehaviour
+{
+    [HideInInspector]
+    public CarController carController;
+
+
+}
---
+++ not-mario-kart/Assets/Scripts/RaceTrack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceTrack : MonoBehaviour
+{
+    [HideInInspector]
+    public Checkpoint[] checkpoints;
+
+    public void Awake()
+    {
+        // get checkpoints
+        this.checkpoints = this.GetComponentsInChildren<Checkpoint>();
+        // register to checkpoints  
+        foreach (Checkpoint checkpoint in this.checkpoints)
+        {
+            checkpoint.raceTrack = this;
+        }
+    }
+}
